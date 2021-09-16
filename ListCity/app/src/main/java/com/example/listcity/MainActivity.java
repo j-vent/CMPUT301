b741857bcf16a0cdb563d37c03002e8f927ce07f
Justine Ventura <jvent@venturazure.onmicrosoft.com>
2021-09-16 17:58:24 -0600

    clean up code

--- ListCity/app/src/main/java/com/example/listcity/MainActivity.java
+++ ListCity/app/src/main/java/com/example/listcity/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<String> dataList;
     ArrayList<String> cities;
     private Button addBtn;
+    // global var to denote city selected in listview
     String selectedCity;
 
     @Override
@@ -28,17 +30,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // a. reference listview
+        // reference listview
         cityView = findViewById(R.id.list_view);
 
-        // b.create string array
+        // create initial string arraylist (mutable unlike string array)
         cities = new ArrayList<>();
         cities.add("Edmonton");
         cities.add("Toronto");
         cities.add("Montreal");
         cities.add("Vancouver");
 
-        // c./d. instantiate dataList and add to datalist
+        // instantiate dataList and populate
         dataList = new ArrayList<>();
         dataList.addAll(cities);
 
@@ -46,65 +48,62 @@ public class MainActivity extends AppCompatActivity {
         cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
         cityView.setAdapter(cityAdapter);
 
-        // setup add button
+        // listen for add button click
         addBtn = (Button) findViewById(R.id.addBtn);
         addBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                addCity();
-                cityAdapter.notifyDataSetChanged();
+                // show edittext and confirm button
+                setVisibilityAddCityComponents(true);
             }
         });
+
+        // add new city to listview
         Button confirmBtn = (Button) findViewById(R.id.confirmBtn);
         confirmBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 EditText cityText = (EditText) findViewById(R.id.city_editText);
-                // TODO: add condition to only add if textbox is not empty
-                dataList.add(cityText.getText().toString());
-                cityAdapter.notifyDataSetChanged();
+                String cityToAdd = cityText.getText().toString();
+                if(cityToAdd.length() > 0){
+                    dataList.add(cityToAdd);
+                    // notify adapter that a new city was added
+                    cityAdapter.notifyDataSetChanged();
+                    // hides keyboard
+                    cityText.onEditorAction(EditorInfo.IME_ACTION_DONE);
+                    // hides add city edittext + confirm button
+                    setVisibilityAddCityComponents(false);
+                }
             }
         });
 
+        // update which city is selected
         cityView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // TextView textView = (TextView) view.findViewById(R.id.content);
-                // String selectedCity = textView.getText().toString();
-                // change colour to denote selection
                 selectedCity = (String) (cityView.getItemAtPosition(position));
-                System.out.println("selected " + selectedCity);
-
             }
         });
 
+        // listen for delete button click
         Button deleteBtn = (Button) findViewById(R.id.delBtn);
         deleteBtn.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
-                // TODO: add condition to only deleted if selected city is not empty
+            public void onClick(View view){
                 dataList.remove(selectedCity);
+                // notify adapter that a city was deleted
                 cityAdapter.notifyDataSetChanged();
             }
         });
-
-
     }
-    public void addCity(){
-        // toggle visibility
-        EditText cityText = (EditText) findViewById(R.id.city_editText);
-        cityText.setVisibility(View.VISIBLE);
-        Button confirmBtn = (Button) findViewById(R.id.confirmBtn);
-        confirmBtn.setVisibility(View.VISIBLE);
-//        confirmBtn.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View view) {
-//                dataList.add(cityText.getText().toString());
-//                // cities.add("justine");
-//
-//            }
-//        });
 
+    /** toggle visibility of editText and confirm button **/
+    public void setVisibilityAddCityComponents(boolean isVisible){
+        int visibilityCode = (isVisible) ? View.VISIBLE : View.INVISIBLE;
+        EditText cityText = (EditText) findViewById(R.id.city_editText);
+        cityText.setVisibility(visibilityCode);
+        Button confirmBtn = (Button) findViewById(R.id.confirmBtn);
+        confirmBtn.setVisibility(visibilityCode);
     }
 
 
